a110d7e41034522af26b2fb6381ca000f1d484f3
Victor Anastacio <victor2002almeida@gmail.com>
2025-06-07 15:36:23 -0300

    Delete flet directory

--- flet/campo_texto.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def principal(page: ft.Page):
-    texto = ft.TextField(label="Digite algo aqui")
-    page.add(texto)
-
-
-
-ft.app(target=principal)
--- flet/texto.py
+++
@@ -1,9 +0,0 @@
-import flet as ft
-
-def principal(page: ft.Page):
-
-
-    texto = ft.Text(value="Esse é um texto de exemplo.")
-    page.add(texto)
-
-ft.app(target=principal)
